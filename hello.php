e65b59a09fb8626d8bd09b14752795e92a62e0cf
Brian Anderson <bqanderson@me.com>
2017-03-10 21:11:44 -0600

    Add DocBlock comment.

--- hello.php
+++ hello.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * A walkthrough of basic the PHP language, ranging from basic statements to conditionals.
+ * I'll be creating a simple "Unit Converter" as well as a "Daily Exercise Program".
+ * I'll then finish up by combining those programs with HTML to create a personal
+ * webpage to demonstrate your skills.
+ *
+ * Author: Brian Q Anderson
+ *
+ * @source Alena Holligan, PHP Teacher at Treehouse
+ * 
+*/
   // My first PHP script!
   echo 'Hello World!'; //Done
 
